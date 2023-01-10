4debaab853164a7d9a1dff3f2ce2224a9091f1f7
myukang <myunghwan0421@gmail.com>
2023-01-11 00:30:32 +0900

    FEAT: echo "git push"

--- envhistorydelete.sh
+++ envhistorydelete.sh
@@ -1,2 +1,3 @@
 read -p "Enter file to delete in git history like .env etc" files
 git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch $files" HEAD
+echo "please command git push origin {remote branch}"
