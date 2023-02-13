7a9b3121d42dbe0ca6bd9f3669ede84bd25c2108
myukang <myunghwan0421@gmail.com>
2023-02-14 03:47:13 +0900

    feat(add node): commit

---
+++ serverInstallUbuntu.sh
@@ -0,0 +1,25 @@
+#update apt
+sudo apt update && sudo apt upgrade
+
+#add node repo
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
+#install
+sudo apt-get install nodejs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
