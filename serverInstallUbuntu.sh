664017c1e06a2fdc8c3b9d7026c41cf8e7259167
koreanddinghwan <myunghwan0421@gmail.com>
2023-02-14 17:27:55 +0900

    feat(add ubuntu script): commit

--- serverInstallUbuntu.sh
+++ serverInstallUbuntu.sh
@@ -1,3 +1,7 @@
+#/bin/bash
+
+#for ubuntu20
+
 #update apt
 sudo apt update && sudo apt upgrade
 
@@ -7,10 +11,20 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 #install
 sudo apt-get install nodejs
 
+#install code deploy
+wget https://aws-codedeploy-ap-northeast-2.s3.amazonaws.com/latest/install
 
+#change execution auth install 
+chmod +x ./install
 
+#install ruby(CodeDeploy Agent is made of ruby)
+sudo apt install ruby -y
 
+#install codedeploy agent
+sudo ./install auto
 
+#check status of codedeploy-agent
+sudo service codedeploy-agent status
 
 
 
