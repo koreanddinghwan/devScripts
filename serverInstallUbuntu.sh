229afcb27d6ab0b91d9ef6f409e398700cae3fe6
koreanddinghwan <myunghwan0421@gmail.com>
2023-02-16 17:59:35 +0900

    feat(ubuntunode18)

--- serverInstallUbuntu.sh
+++ serverInstallUbuntu.sh
@@ -1,9 +1,21 @@
 #/bin/bash
 
 #for ubuntu20
+sudo apt update
 
-#update apt
-sudo apt update && sudo apt upgrade
+sudo apt install wget -y
+sudo apt-get update
+sudo apt-get install ruby-full ruby-webrick wget -y
+cd /tmp
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb
+mkdir codedeploy-agent_1.3.2-1902_ubuntu22
+dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22
+sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control
+dpkg-deb -b codedeploy-agent_1.3.2-1902_ubuntu22/
+sudo dpkg -i codedeploy-agent_1.3.2-1902_ubuntu22.deb
+systemctl list-units --type=service | grep codedeploy
+sudo service codedeploy-agent status
+
 
 #add node repo
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
@@ -11,29 +23,6 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 #install
 sudo apt-get install nodejs
 
-#install code deploy
-wget https://aws-codedeploy-ap-northeast-2.s3.amazonaws.com/latest/install
-
-#change execution auth install 
-chmod +x ./install
-
-#install ruby(CodeDeploy Agent is made of ruby)
-sudo apt install ruby -y
-
-#install codedeploy agent
-sudo ./install auto
 
 #check status of codedeploy-agent
 sudo service codedeploy-agent status
-
-
-
-
-
-
-
-
-
-
-
-
